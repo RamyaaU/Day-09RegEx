5fd8ff31519c910893a38a0bd187505ad0a26ecf
Ramya.U <ramyayadav2016@gmail.com>
2020-10-26 19:09:18 +0530

    [Ramya] Add . Added UC_1 Valid first name

--- Day-09RegEx/Program.cs
+++ Day-09RegEx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Day_09RegEx
 {
@@ -6,7 +7,25 @@ namespace Day_09RegEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //variable
+            string firstName;
+            //pattern 
+            var regex = "^[A-Z]{1}[a-z]{2,20}$";
+            //userinput
+            Console.WriteLine("Enter first name: ");
+            firstName = Console.ReadLine();
+            var match = Regex.Match(firstName, regex);
+            //if pattern matches with the firstname of regex
+            //then it is a valid first name
+            //else invalid first name
+            if (match.Success)
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name");
+            }
         }
     }
 }
