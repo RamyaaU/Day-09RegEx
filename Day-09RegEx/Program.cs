344285089c3fa3b9db66b722d145d501ea332ed1
Ramya.U <ramyayadav2016@gmail.com>
2020-10-26 19:36:01 +0530

    [Ramya] Add . Added UC - 2 Valid Last name

--- Day-09RegEx/Program.cs
+++ Day-09RegEx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Day_09RegEx
 {
@@ -6,7 +7,41 @@ namespace Day_09RegEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //variable
+            string firstName, lastName;
+            //pattern
+            var regex = @"^[A-Z][a-z]{2,}$";
+            //userinput
+            Console.WriteLine("Enter First Name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name: ");
+            lastName = Console.ReadLine();
+            //var match = Regex.Match(string input, string pattern)
+            var matchFirstName = Regex.Match(firstName, regex);
+            //if pattern matches with the firstname of regex
+            //then it is a valid first name
+            //else invalid first name
+            if (matchFirstName.Success)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+            //var match = Regex.Match(string input, string pattern)
+            var matchLastName = Regex.Match(lastName, regex);
+            //if pattern matches with the firstname of regex
+            //then it is a valid last name
+            //else invalid last name
+            if (matchLastName.Success)
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name");
+            }
         }
     }
 }
